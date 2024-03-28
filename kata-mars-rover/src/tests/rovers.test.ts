6a76a985d0daf8a94c4addab0f2f05bb91b5f1d2
Idoia Rojo Lazaro <idoia@ifeelonline.com>
2024-03-28 16:51:23 +0000

    firsts cases and green test

--- kata-mars-rover/src/tests/rovers.test.ts
+++ kata-mars-rover/src/tests/rovers.test.ts
@@ -1,8 +1,25 @@
-import { sum } from '../core/sum';
+type Direction = 'N' | 'E' | 'S' | 'W';
+type Command = 'L' | 'R' | 'M';
+class Rover {
+	constructor(
+		private readonly position: [number, number],
+		private readonly direction: Direction
+	) {}
 
-test('should sum two numbers', () => {
-	const result = sum(1, 2);
-	const expected = 3;
+	static create(position: [number, number], direction: Direction) {
+		return new Rover(position, direction);
+	}
+	getPosition() {
+		return `${this.position[0]}:${this.position[1]}:${this.direction}`;
+	}
+	nextPosition(commands: Command[]) {
+		return Rover.create([0, 0], 'W');
+	}
+}
 
-	expect(result).toBe(expected);
+describe('Mars rover', () => {
+	it('Should create a matrix', () => {
+		const rover = Rover.create([0, 0], 'N');
+		expect(rover.nextPosition(['L']).getPosition()).toBe('0:0:W');
+	});
 });
